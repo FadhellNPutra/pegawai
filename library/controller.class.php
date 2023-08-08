116bd639948307bb5ebdb342ab87094de9e1dafc
FadhellNPutra <fadhellnandaputra@gmail.com>
2023-08-08 13:28:15 +0700

    pakai framework dari buku 7in1 lanjut

---
+++ library/controller.class.php
@@ -0,0 +1,24 @@
+<?php 
+    class Controller{
+        protected function view($viewName){
+            return new View($viewName);
+        }
+        protected function model($modelName){
+            require_once ROOT . "/application/models" . $modelName . ".model.php";
+        }
+        protected function template($viewName, $data = array()){
+            $view = $this->view('template');
+            $view->bind('viewName', $viewName);
+            $view->bind('data', $data);
+        }
+        public function back(){
+            echo '<script>history.go(-1)</script>';
+        }
+        public function redirect($url = ""){
+            header("Location: ". BASE_PATH.DS.$url);
+        }
+        protected function validate($data){
+            return htmlentities(trim(strip_tags($data)));
+        }
+    }
+?>
